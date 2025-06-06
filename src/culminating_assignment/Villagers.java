3c7aeb768173ad2a988446b912de08b0ff518ecf
china <china@10.0.0.59>
2025-06-06 08:27:15 -0400

    add villagers class

--- src/culminating_assignment/Villagers.java
+++ src/culminating_assignment/Villagers.java
@@ -3,12 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package culminating_assignment;
-import processing.core.PApplet;
-import processing.core.PImage;
+
 /**
  *
  * @author Zi Cheng Qiu
  */
-public class villager {
+public class Villagers {
     
 }
